710abc5bfbd0a60b9a6f59eb39c6741669b9201d
SimonGideon <simongideon918@gmail.com>
2022-01-28 11:17:24 -0500

    first commit

---
+++ Index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pharmacy System</title>
+    <!-- css link -->
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+<br>
+<br>
+<div class="container">
+    <div id="login-form">
+    <img src="assets/images/logo.png" alt="" class="avatar" srcset="">
+        <h3 class="login">User Login</h3>
+            <form id=form-items>
+                <input type="text" autofocus id="un" placeholder="Username" required autocomplete="off">
+                <input type="password" id="up" placeholder="Password" required autocomplete="off">
+                <input class="submit" type="submit" name="log" value="login">
+            </form>
+        <div>
+
+        </div>
+
+    </div>
+
+</div>
+    
+</body>
+</html>
